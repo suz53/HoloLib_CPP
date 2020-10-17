75d62d698a6ae6cc9c149bf2a64f7ff0ee60f394
XuRuLin <xrlin7@gmail.com>
2020-10-17 18:25:20 +0800

    修改size_t转int可能丢失问题

--- HoloLib/CGH_Algorithm/src/meshgrid.cpp
+++ HoloLib/CGH_Algorithm/src/meshgrid.cpp
@@ -10,8 +10,8 @@ int gs::meshgrid(InputArray _xgv, InputArray _ygv, OutputArray _X, OutputArray _
     _Y.create(xgv.rows, ygv.cols, ygv.type());
     Mat X = _X.getMat();
     Mat Y = _Y.getMat();
-    repeat(xgv.reshape(1, 1), ygv.total(), 1, X);
-    repeat(ygv.reshape(1, 1).t(), 1, xgv.total(), Y);
+    repeat(xgv.reshape(1, 1), (int)ygv.total(), 1, X);
+    repeat(ygv.reshape(1, 1).t(), 1, (int)xgv.total(), Y);
     X.copyTo(_X);
     Y.copyTo(_Y);
     return NORMAL;
